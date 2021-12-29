7509b74a315c6492c047136b0eabea9adaabb2bf
Brian Walters <brianw@pixotech.com>
2021-12-29 11:43:42 -0600

    Add flysystem and a simple file listing

---
+++ src/Controller/FileController.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use League\Flysystem\FilesystemOperator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FileController extends AbstractController
+{
+    public const FILE_DIR = 'files';
+
+    #[Route('/files', name: 'file_list')]
+    public function fileList(FilesystemOperator $fileStorage): Response
+    {
+        $directoryListing = $fileStorage->listContents('.');
+
+        return $this->render('pages/file-list.html.twig', [
+            'files' => $directoryListing,
+        ]);
+    }
+}
---
+++ src/Twig/AppExtension.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Twig;
+
+use App\Controller\FileController;
+use Symfony\Component\Asset\Packages;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class AppExtension extends AbstractExtension
+{
+    public function __construct(private Packages $asset)
+    {
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/2.x/advanced.html#automatic-escaping
+            new TwigFilter('fileHref', [$this, 'fileHref']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+//            new TwigFunction('function_name', [$this, 'doSomething']),
+        ];
+    }
+
+    public function fileHref(string $value): string
+    {
+        return $this->asset->getUrl(FileController::FILE_DIR . '/' . $value);
+    }
+}
