b65d0bc0e5fe5be298986a21079c8f2ddaa36378
Brian Walters <brianw@pixotech.com>
2021-12-30 11:27:15 -0600

    Add script for updating a deployment

---
+++ bin/update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git pull
+composer install
+./bin/console cache:warmup -e prod
+chown -R www-data:www-data var
+./bin/console d:m:m -n
+
+HTTPDUSER=$(ps axo user,comm | grep -E '[a]pache|[h]ttpd|[_]www|[w]ww-data|[n]ginx' | grep -v root | head -1 | cut -d\  -f1)
+sudo setfacl -dR -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX var
+sudo setfacl -R -m u:"$HTTPDUSER":rwX -m u:$(whoami):rwX var
