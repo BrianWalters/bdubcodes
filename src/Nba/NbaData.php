9c96d0e3d2b5ee7c5672b5646d349e7fb85df41b
Brian Walters <brian.walters@hey.com>
2025-11-05 22:05:05 -0600

    Reorder standings with individual drafters

--- src/Nba/NbaData.php
+++ src/Nba/NbaData.php
@@ -23,7 +23,7 @@ class NbaData
                     'headers' => [
                         'Host' => 'stats.nba.com',
                         'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv=>72.0) Gecko/20100101 Firefox/72.0',
-                        'Accept' => 'application/json, text/plain, */*',
+                        'Accept' => 'application/json',
                         'Accept-Language' => 'en-US,en;q=0.5',
                         'x-nba-stats-origin' => 'stats',
                         'x-nba-stats-token' => 'true',
